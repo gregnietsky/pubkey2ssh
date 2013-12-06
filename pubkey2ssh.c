c3e9f75c637bbf89a24d266ab8468ca239dacb5d
Paul Ewing <pjewing@securemissionsolutions.com>
2013-12-06 15:46:36 -0600

    Add support for x509 certificates.

--- pubkey2ssh.c
+++ pubkey2ssh.c
@@ -11,11 +11,15 @@
 
 #include <memory.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
 
+#define FORMAT_RAW 100
+#define FORMAT_PEM 101
+#define FORMAT_DER 102
 
 static unsigned char pSshHeader[11] = { 0x00, 0x00, 0x00, 0x07, 0x73, 0x73, 0x68, 0x2D, 0x72, 0x73, 0x61};
 
@@ -40,32 +44,58 @@ static int SshEncodeBuffer(unsigned char *pEncoding, int bufferLen, unsigned cha
    return index + bufferLen;
 }
 
+static void GetOptions(int argc, char **argv, int *iFileType) {
+   int c;
+   while ((c = getopt (argc, argv, "dpr")) != -1)
+      switch (c)
+      {
+         case 'd':
+            *iFileType = FORMAT_DER;
+            break;
+         case 'p':
+            *iFileType = FORMAT_PEM;
+            break;
+         case 'r':
+            *iFileType = FORMAT_RAW;
+            break;
+         default:
+            abort();
+      }
+}
+
 int main(int argc, char**  argv) {
    int iRet = 0;
    int nLen = 0, eLen = 0;
    int encodingLength = 0;
    int index = 0;
+   int iFileType = FORMAT_RAW;
    unsigned char *nBytes = NULL, *eBytes = NULL;
    unsigned char* pEncoding = NULL;
    FILE* pFile = NULL;
    EVP_PKEY *pPubKey = NULL;
+   X509 *pCert = NULL;
    RSA* pRsa = NULL;
    BIO *bio, *b64;
 
    ERR_load_crypto_strings(); 
    OpenSSL_add_all_algorithms();
 
-   if (argc != 3)
+   GetOptions(argc, argv, &iFileType);
+
+   if (argc-optind != 2)
    {
-      printf("usage: %s public_key_file_name ssh_key_description\n", argv[0]);
+      printf("usage: %s [-d|-p|-r] public_key_file_name ssh_key_description\n", argv[0]);
+      printf("   -d   x509 DER\n");
+      printf("   -p   x509 PEM\n");
+      printf("   -r   raw public key\n");
       iRet = 1;
       goto error;
    }
 
-   if (!strcmp(argv[1],"-")) {
+   if (!strcmp(argv[argc-2],"-")) {
      pFile = stdin;
    } else {
-     pFile = fopen(argv[1], "rt");
+     pFile = fopen(argv[argc-2], "rt");
    }
    if (!pFile)
    {
@@ -74,7 +104,30 @@ int main(int argc, char**  argv) {
       goto error;
    }
 
-   pPubKey = PEM_read_PUBKEY(pFile, NULL, NULL, NULL);
+   switch (iFileType) {
+      case FORMAT_RAW:
+         pPubKey = PEM_read_PUBKEY(pFile, NULL, NULL, NULL);
+         break;
+      case FORMAT_PEM:
+         pCert = PEM_read_X509_AUX(pFile, NULL, NULL, NULL);
+         if (!pCert)
+         {
+            printf("Unable to read PEM format from the given file: %s\n", ERR_error_string(ERR_get_error(), NULL));
+            iRet = 6;
+            goto error;
+         }
+         pPubKey = X509_get_pubkey(pCert);
+         break;
+      case FORMAT_DER:
+         pCert = d2i_X509_fp(pFile, NULL);
+         if (!pCert)
+         {
+            printf("Unable to read DER format from the given file: %s\n", ERR_error_string(ERR_get_error(), NULL));
+            iRet = 7;
+            goto error;
+         }
+         pPubKey = X509_get_pubkey(pCert);
+   }
    if (!pPubKey)
    {
       printf("Unable to decode public key from the given file: %s\n", ERR_error_string(ERR_get_error(), NULL));
@@ -128,7 +181,7 @@ int main(int argc, char**  argv) {
    BIO_write(bio, pEncoding, encodingLength);
    BIO_flush(bio);
    bio = BIO_pop(b64);
-   BIO_printf(bio, " %s\n", argv[2]);
+   BIO_printf(bio, " %s\n", argv[argc-1]);
    BIO_flush(bio);
    BIO_free_all(bio);
    BIO_free(b64);
